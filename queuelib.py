c5ebbbbc1502aa42579300c91303c5e6fcbab502
Lilly <lillycat332@gmail.com>
2021-12-08 15:12:05 +0000

    - add code comments for queue lib

--- queuelib.py
+++ queuelib.py
@@ -1,5 +1,13 @@
 from arrayLib import *
  
+class queueFullException(Exception):
+    """raised when a queue is full"""
+    pass
+
+class queueEmptyException(Exception):
+    """raised when a queue is full"""
+    pass
+
 class naiveQueue(Array):
     def __init__(self, l):
         self.__myArray = Array(l)
@@ -22,6 +30,12 @@ class naiveQueue(Array):
             print(self.__myArray.get(i))
 
 class circularQueue():
+    """
+    Circular Queue
+    - Holds n - 1 items
+    - FIFO
+    - head and tail loop from n to 0
+    """
     def __init__(self, l):
         self.__queue = Array(l)
         self.__max = l - 1
@@ -29,32 +43,49 @@ class circularQueue():
         self.__tail = 0
        
     def enqueue(self, newitem):
+        """
+        Enqueue:
+        - Adds newitem to the queue
+        - Moves the tail to accomodate for the new item
+        - Handles the seamless transfer from end of queue to start with the head and tail pointers
+        """
         if ((self.__tail + 1) % self.__max == self.__head):
-            print("The circular queue is full")
+            raise queueFullException()
 
         elif (self.__head == -1):
             self.__head = 0
             self.__tail = 0
             self.queue.assign(self.__tail, newitem)
+
         else:
             self.__tail = (self.__tail + 1) % self.__max
             self.__queue.assign(self.__tail, newitem)
 
     def dequeue(self):
+        """
+        Dequeue:
+        - Dequeues the next item from the queue
+        - Moves the head to accomodate for the removed item
+        - Handles the seamless transfer from end of queue to start with the head and tail pointers
+        """
         if (self.__head == -1):
-            print("circular queue is empty")
+            raise queueEmptyException()
 
         elif (self.__head == self.__tail):
-            v = self.__queue.get(self.__head)
+            r = self.__queue.get(self.__head)
             self.__head = -1
             self.__tail = -1
-            return v
+            return r
+
         else:
-            v = self.__queue.get(self.__head)
+            r = self.__queue.get(self.__head)
             self.__head = (self.__head + 1) % self.__max
-            return v
+            return r
 
     def printqueue(self):
+        """
+        Prints contents of queue for debug purposes
+        """
         for i in range(0, self.__queue.getSize() - 1):
             print(self.__queue.get(i))
  
@@ -69,17 +100,17 @@ class priorityQueue():
        
     def dequeue(self):
         for i in self.__list():
-           
-            self.__list[q].dequeue()
-       
+            if i.get(i) != None:
+                self.__list[q].dequeue()
+
         
     def prin(self):
         for i in self.__list:
             print("\n")
             i.printqueue()
-           
- 
+
 myq = priorityQueue(4, 4)
- 
-myq.enqueue(1, "Cat")
-myq.prin()
+
+while True:
+    myq.enqueue(1, "Cat")
+    myq.prin()
