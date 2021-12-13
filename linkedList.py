a1b98f88c987e936a65cc7d402ea32e22b97c7e2
Lilly <54189319+lillycat332@users.noreply.github.com>
2021-12-13 15:14:55 +0000

    add linked list

---
+++ linkedList.py
@@ -0,0 +1,35 @@
+class listItem():
+    def __init__(self, content, link):
+        self.__link = link
+        self.__content = content
+   
+    def setLink(self, link):
+        self.__link = link
+ 
+class linkedList():
+    def __init__(self):
+        memory = [listItem()]
+ 
+    def pop(self):
+       
+ 
+		def add(self, value):
+       
+ 
+    def shift(self):
+       
+ 
+    def unshift(self):
+       
+ 
+    def remove(self, index):
+       
+ 
+    def insert(self, index):
+ 
+
+    def peek(self, index):
+ 
+
+    def getLength(self):
+       
