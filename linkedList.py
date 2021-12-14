2fb59662a4d3eb570f178c59aeed0713509a96f3
Lilly <54189319+lillycat332@users.noreply.github.com>
2021-12-14 08:18:11 +0000

    i frogor

--- linkedList.py
+++ linkedList.py
@@ -1,5 +1,12 @@
 class listItem():
-    def __init__(self, content, link):
+    """
+    List Item
+    - holds a pointer to the listItem that is connected to, and the content held within the item
+    - allows changing content and link with setLink and setContent
+    - this would probably be better with a struct, but python doesn't have those for some reason.
+    """
+
+    def __init__(self, content, link=None):
         self.__link = link
         self.__content = content
    
@@ -8,28 +15,29 @@ class listItem():
  
 class linkedList():
     def __init__(self):
-        memory = [listItem()]
+        self.__memory = [listItem()]
  
     def pop(self):
-       
+        self.__memory[(len(self.__memory) - 1)].setLink(len(None))
  
-		def add(self, value):
-       
+    def add(self, value):
+        self.__memory[(len(self.__memory) - 1)].setLink(len(self.__memory))
+        self.__memory.append(listItem(value))
  
     def shift(self):
-       
+        pass
  
     def unshift(self):
-       
+        pass
  
     def remove(self, index):
-       
+        pass
  
     def insert(self, index):
- 
+        pass
 
     def peek(self, index):
- 
+        pass
 
     def getLength(self):
-       
+        pass
