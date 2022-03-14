5869337bcde93dcc6407aba9e1f17298fa956d43
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-03-14 13:53:11 +0000

    bfs

--- binarytodecimal.py
+++ binarytodecimal.py
@@ -1,11 +1,11 @@
 def BinaryToDecimal(binary):
-    decimal = 0 # Decimal, this is what we return
-    count = 0 # count for power
-    while binary: # while binary is not equal to zero
-        digit = binary % 10 # digit = binary mod 10 (10 for decimal)
+    decimal = 0 
+    count = 0 
+    while binary: 
+        digit = binary % 10 
         decimal = decimal + digit * pow(2 , count)
-        binary = binary//10 # integer divide by 10
-        count += 1 # add 1 to count for power operator
+        binary = binary//10 
+        count += 1              
     return decimal
 binary = int(input("Enter a binary number: "))
 print(BinaryToDecimal(binary))
--- dfs.py
+++ dfs.py
@@ -1,21 +1,64 @@
-from typing import *
-
+import stackLib; from typing import *; import pyfiglet
+ 
 class dfs():
-	def __init__(self) -> None:
-		self.__visited : Set[Any] = set()
+	def __init__(self, maze):
+		self.__maze = maze
+		self.__stack = stackLib.Stack(len(maze))
+		self.__visited = [False] * len(maze)
+ 
+	def depthFirst(self) -> None:
+		"""
+		Depth first traversal
+		args:
+			- graph: an adjacency list (Dictionary) of a graph
+			- node: the starting node (Any)
+		returns: Set[Any]
+		"""
+		start = 8
+		finished = False
+		current = start
+		while not finished:
+			try:
+				print(pyfiglet.figlet_format(current))
+				if not (self.__nodeComplete(current)):
+					self.__stack.push(current)
+ 
+				self.__visited[current] = True
+				next = self.__findNextNode(current)
+ 
+				if next != None:
+					current = next
+				else:
+					current = self.__stack.pop()
+					print("Deadend: returning to", current, end='\n')
+				self.__stack.printArray()
+				input()
+			except:
+				print("Done")
 
-	def depthFirstRecursive(self, graph, node) -> Set[Any]:
-		"""
-		Depth first traversal - recursive
-		"""
-		if node not in self.__visited:
-			self.__visited.add(node)
-			print(node)
-			for neighbor in graph[node]:
-				self.depthFirstRecursive(graph, neighbor)
-		return self.__visited
+	def __findNextNode(self, node) -> node:
+		#finds next unvisited node in the adjacency list
+		nodes = self.__maze[node]
+		i = 0
+		while i<len(nodes):
+			if not(self.__visited[ nodes[i] ]):
+				return self.__maze[node][ i ]
+			else:
+				i += 1
+		return None
+
+	def __nodeComplete(self, node) -> Bool:
+	#returns true if node p has been fully explored
+		nodes = self.__maze[node]
+		i = 0
+		while i<len(nodes):
+			if not(self.__visited[ nodes[i] ]):
+				return False
+			else:
+				i += 1
+		return True
 
 maze = {0: [1,5,4], 1:[2,0], 2:[3,1,6], 3:[2], 4:[0,8], 5:[0,6,9,8], 6:[2,7,9,5], 7:[6], 8:[4,5,9], 9:[8,5,6]}
-inst = dfs()
-
-print("Visited nodes: %s" % inst.depthFirstRecursive(maze, 0))
+inst = dfs(maze)
+print(pyfiglet.figlet_format("Depth \n First \n Search"))
+print("Visited nodes: %s"% inst.depthFirst())
