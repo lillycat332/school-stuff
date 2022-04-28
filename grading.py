0c55dfa8cf5e55c3864fa9b5511095ddba3456e6
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-29 00:36:51 +0100

    Delete grading.py

--- grading.py
+++
@@ -1,9 +0,0 @@
-def marktograde(m):
-  grade = ""
-  if m>=70:grade="A"
-  elif m>=60 and m<=69:grade="B"
-  elif m>=50 and m<59:grade="C"
-  elif m>=40 and m<=49:grade="D"
-  return(grade)
-mark = int(input("Input mark: "))
-print(marktograde(mark))
